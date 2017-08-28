1f030052985031b48f44a8d3789b8bb1e5211157
Fabian Bühler <st116290@stud.uni-stuttgart.de>
2017-08-28 14:53:05 +0200

    better urls for routes overview

--- muse_for_music/debug_routes/routes.py
+++ muse_for_music/debug_routes/routes.py
@@ -9,24 +9,13 @@ def routes():
     output = []
     for rule in app.url_map.iter_rules():
 
-        options = {}
-        print(rule.arguments)
-        for arg in rule.arguments:
-            if 'id' in arg:
-                options[arg] = 0
-                continue
-            options[arg] = "{{{0}}}".format(arg)
-
-        methods = ', '.join(rule.methods)
-        url = url_for(rule.endpoint, **options)
         line = {
             'endpoint': rule.endpoint,
-            'methods': methods,
-            'url': urllib.parse.unquote(url),
-            'args': str(rule.arguments) if rule.arguments else '',
+            'methods': ', '.join(rule.methods),
+            'url': rule.rule
         }
         output.append(line)
-        output.sort(key=lambda x: x['url'])
+    output.sort(key=lambda x: x['url'])
     return render_template('debug/routes/all.html',
                            title='muse4music – Routes',
                            routes=output)
