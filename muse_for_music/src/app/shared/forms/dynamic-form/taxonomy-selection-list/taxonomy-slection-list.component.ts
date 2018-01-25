ef920a86c1275a3dd9a05ea297a21aa11ec61443
Fabian Bühler <st16290@stud.uni-stuttgart.de>
2018-01-25 23:36:53 +0100

    working tree select

--- muse_for_music/src/app/shared/forms/dynamic-form/taxonomy-selection-list/taxonomy-slection-list.component.ts
+++ muse_for_music/src/app/shared/forms/dynamic-form/taxonomy-selection-list/taxonomy-slection-list.component.ts
@@ -8,8 +8,9 @@ class TaxonomyItem {
     name: string;
     description?: string;
     data: any;
-    depth?: number;
-    children?: TaxonomyItem[];
+    depth: number;
+    parents: number[];
+    children: number[];
 
     constructor(data: any, depth?: number) {
         this.id = data.id;
@@ -17,6 +18,7 @@ class TaxonomyItem {
         this.description = data.description;
         this.data = data;
         this.depth = 0;
+        this.parents = [];
         this.children = [];
         if (depth != undefined) {
             this.depth = depth;
@@ -33,6 +35,8 @@ class TaxonomyItem {
 export class TaxonomySelectionListComponent implements OnChanges, OnInit {
 
     isTree: boolean;
+    filter: boolean = true;
+    showTree: boolean = false;
 
     selectables: TaxonomyItem[];
 
@@ -42,6 +46,9 @@ export class TaxonomySelectionListComponent implements OnChanges, OnInit {
     @Input() search: string;
     @Input() _selected: any[];
     @Output() selectedChange = new EventEmitter();
+
+    highlightable: number[] = [];
+    closed: Set<number> = new Set<number>();
 
     highlighted: number = undefined;
     selectedSet: Set<number> = new Set<number>();
@@ -66,6 +73,30 @@ export class TaxonomySelectionListComponent implements OnChanges, OnInit {
         this.selectedSet = selected;
     }
 
+    visible(selectable: TaxonomyItem): boolean {
+        if (!this.filter) {
+            return true;
+        }
+        if (this.showTree) {
+            for (let parent of selectable.parents) {
+                if (this.closed.has(parent)) {
+                    return false;
+                }
+            }
+            if (this.matching.has(selectable.id)) {
+                return true;
+            }
+            for (let child of selectable.children) {
+                if (this.matching.has(child)) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            return this.matching.has(selectable.id);
+        }
+    }
+
     constructor(private api: ApiService) {}
 
     ngOnChanges(changes: SimpleChanges): void {
@@ -79,11 +110,14 @@ export class TaxonomySelectionListComponent implements OnChanges, OnInit {
             }
             if (taxonomy.taxonomy_type === 'tree') {
                 this.isTree = true;
+                this.showTree = true;
                 this.prepareTreeTaxonomy(taxonomy.items);
             } else {
                 this.isTree = false;
+                this.showTree = false;
                 this.prepareListTaxonomy(taxonomy.items);
             }
+            this.closed.clear();
             this.updateMatching();
         });
     }
@@ -110,6 +144,12 @@ export class TaxonomySelectionListComponent implements OnChanges, OnInit {
 
         let node = new TaxonomyItem(item, parents.size);
         flatList.push(node);
+
+        parents.forEach(parent => {
+            parent.children.push(node.id);
+            node.parents.push(parent.id);
+        });
+
         parents.add(node);
         for (let child of children) {
             this.prepareTreeTaxonomy(child, flatList, parents);
@@ -133,11 +173,52 @@ export class TaxonomySelectionListComponent implements OnChanges, OnInit {
 
         for (let item of this.selectables) {
             if (item.name.toUpperCase().includes(searchString) ||
-                item.description.toUpperCase().includes(searchString)) {
+                item.description.toUpperCase().includes(searchString) ||
+                (item.name === 'root' && this.taxonomy.toUpperCase().includes(searchString))) {
                 matches.add(item.id);
             }
         }
         this.matching = matches;
+        this.updateHighlightable();
+    }
+
+    updateHighlightable() {
+        let items: number[] = [];
+        let highlightedStillValid = false;
+        for (let item of this.selectables) {
+            if (!this.filter || this.matching.has(item.id)) {
+                if (this.showTree) {
+                    if (this.visible(item)) {
+                        items.push(item.id);
+                        if (item.id === this.highlighted) {
+                            highlightedStillValid = true;
+                        }
+                    }
+                } else {
+                    items.push(item.id);
+                    if (item.id === this.highlighted) {
+                        highlightedStillValid = true;
+                    }
+                }
+            }
+        }
+
+        this.highlightable = items;
+
+        if (!highlightedStillValid && items.length > 0) {
+            this.highlighted = items[0];
+        }
+    }
+
+    toggleClosed(selectable: TaxonomyItem) {
+        if (selectable.children.length > 0) {
+            if (this.closed.has(selectable.id)) {
+                this.closed.delete(selectable.id);
+            } else {
+                this.closed.add(selectable.id);
+            }
+        }
+        this.updateHighlightable();
     }
 
     deselect(key?: any) {
@@ -169,8 +250,27 @@ export class TaxonomySelectionListComponent implements OnChanges, OnInit {
     }
 
     highlightNext() {
+        let searching = true;
+        for (let key of this.highlightable) {
+            if (searching && key === this.highlighted){
+                searching = false;
+                continue;
+            }
+            if (!searching) {
+                this.highlighted = key;
+                return;
+            }
+        }
     }
 
     highlightPrevious() {
+        let previous: number = this.highlighted;
+        for (let key of this.highlightable) {
+            if (key === this.highlighted){
+                this.highlighted = previous;
+                return;
+            }
+            previous = key;
+        }
     }
 }
