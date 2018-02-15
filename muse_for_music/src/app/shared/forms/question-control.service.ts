b6d120ed0263e62bc9f7adba253ee646a2b13bc0
Fabian Bühler <st116290@stud.uni-stuttgart.de>
2018-02-15 15:05:47 +0100

    better validators

--- muse_for_music/src/app/shared/forms/question-control.service.ts
+++ muse_for_music/src/app/shared/forms/question-control.service.ts
@@ -11,8 +11,45 @@ export class QuestionControlService {
         let group: any = {};
 
         questions.forEach(question => {
-            group[question.key] = question.required ? new FormControl(question.value || '', Validators.required)
-                : new FormControl(question.value || '');
+            let validators = [];
+            if (question.required) {
+                if (question.controlType === 'boolean') {
+                    // nothing
+                } else if (question.controlType === 'string' || question.controlType === 'text') {
+                    if (question.min != undefined && question.min === 1) {
+                        validators.push(Validators.required);
+                    }
+                } else {
+                    validators.push(Validators.required);
+                }
+            }
+            if (question.min != undefined) {
+                if (question.controlType === 'number') {
+                    validators.push(Validators.min(question.min as number));
+                }
+                if (question.controlType === 'string' || question.controlType === 'text') {
+                    if (question.min > 1) {
+                        validators.push(Validators.minLength(question.min as number))
+                    }
+                }
+            }
+            if (question.max != undefined) {
+                if (question.controlType === 'number') {
+                    validators.push(Validators.max(question.max as number));
+                }
+                if (question.controlType === 'string' || question.controlType === 'text') {
+                    validators.push(Validators.maxLength(question.max as number))
+                }
+            }
+
+            if (validators.length > 1) {
+                const validator = Validators.compose(validators);
+                group[question.key] = new FormControl(question.value || '', validator)
+            } else if (validators.length === 1) {
+                group[question.key] = new FormControl(question.value || '', validators[0])
+            } else {
+                group[question.key] = new FormControl(question.value || '');
+            }
         });
         return new FormGroup(group);
     }
