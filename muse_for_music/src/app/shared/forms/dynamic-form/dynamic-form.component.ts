a16c98e1d766f8538d5096f7d4e8b6217ea98082
Fabian Bühler <st16290@stud.uni-stuttgart.de>
2018-04-12 00:28:27 +0200

    remove custom null from dynamic form

--- muse_for_music/src/app/shared/forms/dynamic-form/dynamic-form.component.ts
+++ muse_for_music/src/app/shared/forms/dynamic-form/dynamic-form.component.ts
@@ -18,7 +18,6 @@ export class DynamicFormComponent implements OnInit, OnChanges {
     @Input() startValues: ApiObject = {_links:{self:{href:''}}};
 
     questions: QuestionBase<any>[] = [];
-    customNull: {[propName: string]: any} = {};
     form: FormGroup;
 
     @Output() valid: EventEmitter<boolean> = new EventEmitter<boolean>();
@@ -29,45 +28,12 @@ export class DynamicFormComponent implements OnInit, OnChanges {
     update() {
         this.qs.getQuestions(this.objectModel).subscribe(questions => {
             this.questions = questions;
-            this.customNull = {};
-
-            function recursiveParse(questions, customNull, qs) {
-                for (const question of questions) {
-                    if (question.nullValue != undefined) {
-                        customNull[question.key] = question.nullValue;
-                    }
-                    if (question.controlType === 'object') {
-                        const temp1 = {$continuation:true};
-                        const temp2 = {$continuation:true};
-                        customNull[question.key] = temp1;
-                        qs.getQuestions(question.valueType).subscribe(questions => recursiveParse(questions, temp1, qs))
-                    }
-                }
-            }
-            recursiveParse(questions, this.customNull, this.qs);
 
             this.qcs.toFormGroup(this.questions).subscribe(form => {
                 this.form = form;
                 this.form.statusChanges.subscribe(status => {
                     this.valid.emit(this.form.valid);
-                    let patched = {};
-
-                    function recursivePatchNulls(value, patched, customNull) {
-                        for (let key in value) {
-                            if (customNull[key] != null && customNull[key]['$continuation']) {
-                                patched[key] = {};
-                                recursivePatchNulls(value[key], patched[key], customNull[key]);
-                            } else if (value[key] != null && value[key] !== '') {
-                                patched[key] = value[key];
-                            } else {
-                                patched[key] = customNull[key];
-                            }
-                        }
-                    }
-
-                    recursivePatchNulls(this.form.value, patched, this.customNull);
-
-                    this.data.emit(patched);
+                    this.data.emit(this.form.value);
                 });
                 this.patchFormValues();
             });
@@ -75,27 +41,8 @@ export class DynamicFormComponent implements OnInit, OnChanges {
     }
 
     patchFormValues() {
-        let patched = {};
-
-        function recursivePatch(values, patched, customNull) {
-            for (let key in values) {
-                if (customNull[key] != null && customNull[key]['$continuation']) {
-                    patched[key] = {};
-                    recursivePatch(values[key], patched[key], customNull[key]);
-                } else if (customNull[key] === values[key]) {
-                    patched[key] = null;
-                } else if (customNull[key] != null && typeof customNull[key] === 'object' && customNull[key].id === values[key].id) {
-                    patched[key] = null;
-                } else {
-                    patched[key] = values[key];
-                }
-            }
-        }
-
-        recursivePatch(this.startValues, patched, this.customNull);
-
-        if (this.form != undefined) {
-            this.form.patchValue(patched);
+        if (this.form != null) {
+            this.form.patchValue(this.startValues);
         }
     }
 
@@ -104,10 +51,10 @@ export class DynamicFormComponent implements OnInit, OnChanges {
     }
 
     ngOnChanges(changes: SimpleChanges): void {
-        if (changes.objectType != undefined) {
+        if (changes.objectType != null) {
             this.update();
         } else {
-            if (this.form != undefined && changes.startValues != undefined) {
+            if (this.form != null && changes.startValues != null) {
                 this.patchFormValues();
             }
         }
