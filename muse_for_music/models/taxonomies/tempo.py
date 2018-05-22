35e441162390a7c0a41645c6d5c2ae7d40a16032
Fabian Bühler <st16290@stud.uni-stuttgart.de>
2018-05-23 00:03:12 +0200

    updated taxonomies

--- muse_for_music/models/taxonomies/tempo.py
+++ muse_for_music/models/taxonomies/tempo.py
@@ -6,13 +6,21 @@ from .helper_classes import TreeTaxonomy, ListTaxonomy
 __all__ = ['Tempo', 'TempoEntwicklung', 'TempoEinbettung']
 
 
-class Tempo(db.Model, ListTaxonomy):
+class Tempo(db.Model, TreeTaxonomy):
     """DB Model for choices."""
     __tablename__ = 'tempo'
 
     id = db.Column(db.Integer, primary_key=True)
+    parent_id = db.Column(db.Integer, db.ForeignKey('tempo.id', ondelete='CASCADE'))
     name = db.Column(db.String(120))
     description = db.Column(db.Text(), nullable=True)
+    children = db.relationship('Tempo',
+                               backref=db.backref('parent',
+                                                  remote_side=[id],
+                                                  lazy='joined',
+                                                  join_depth=1
+                                                 )
+                              )
 
 
 class TempoEntwicklung(db.Model, ListTaxonomy):
